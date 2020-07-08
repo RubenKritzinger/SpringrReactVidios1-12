f9cf53935fa68c08c074161a54dfc42933fff3c4
RubenKritzinger <rubenkritzinger1998@gmail.com>
2020-07-08 10:44:23 +0200

    finnised section 2

---
+++ src/components/learning-examples/ThirdComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//Class Component
+ function ThirdComponent(){
+    return (
+      <div className="ThirdComponent">
+      Third Component
+      </div>  
+    );
+  }
+
+  export default ThirdComponent
